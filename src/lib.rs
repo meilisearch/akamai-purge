1a5a8cd353373ff18ef776826efce9f9087a151b
Clément Renault <renault.cle@gmail.com>
2019-02-28 11:04:31 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+use std::fmt;
+
+use chrono::prelude::*;
+use hmac::{Hmac, Mac};
+use sha2::{Sha256, Digest};
+use url::Url;
+use uuid::Uuid;
+
+const TIMESTAMP_FORMAT: &str = "%Y%m%dT%H:%M:%S+0000";
+
+type HmacSha256 = Hmac<Sha256>;
+
+fn signing_key(client_secret: &str, now: DateTime<Utc>) -> String {
+    let timestamp = now.format(TIMESTAMP_FORMAT).to_string();
+    let mut mac = HmacSha256::new_varkey(client_secret.as_bytes()).unwrap();
+    mac.input(timestamp.as_bytes());
+    base64::encode(&mac.result().code())
+}
+
+fn signature(signing_key: &str, data_to_sign: &str) -> String {
+    let mut mac = HmacSha256::new_varkey(signing_key.as_bytes()).unwrap();
+    mac.input(data_to_sign.as_bytes());
+    base64::encode(&mac.result().code())
+}
+
+fn body_hash(body: &[u8]) -> String {
+    let mut hasher = Sha256::new();
+    hasher.input(body);
+    base64::encode(&hasher.result())
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum HttpMethod {
+    Get,
+    Post,
+}
+
+impl fmt::Display for HttpMethod {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HttpMethod::Get => f.write_str("GET"),
+            HttpMethod::Post => f.write_str("POST"),
+        }
+    }
+}
+
+pub fn authorization_header(
+    method: HttpMethod,
+    url: &Url,
+    body: Option<&[u8]>,
+    access_token: &str,
+    client_token: &str,
+    client_secret: &str,
+) -> String
+{
+    let now_timestamp = Utc::now();
+    let signing_key = signing_key(client_secret, now_timestamp);
+
+    let nonce = Uuid::new_v4().to_hyphenated();
+
+    let mut auth_data =
+        format!("EG1-HMAC-SHA256 client_token={};access_token={};timestamp={};nonce={};",
+            client_token,
+            access_token,
+            now_timestamp.format(TIMESTAMP_FORMAT).to_string(),
+            nonce,
+        );
+
+    let data_to_sign = {
+        let method = method.to_string();
+        let scheme = url.scheme();
+        let host = url.host_str().expect("HOST not found");
+        let relurl = url.path();
+        let mut data_to_sign = format!("{}\t{}\t{}\t{}\t\t", method, scheme, host, relurl);
+        if let Some(body) = body {
+            let body_hash = body_hash(body);
+            data_to_sign.push_str(&body_hash);
+        }
+        data_to_sign.push('\t');
+        data_to_sign.push_str(&auth_data);
+        data_to_sign
+    };
+
+    let signature = signature(&signing_key, &data_to_sign);
+    auth_data.push_str(&format!("signature={}", signature));
+
+    auth_data
+}
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use reqwest::{header, Client};
+use url::Url;
+
+use akamai::{authorization_header, HttpMethod};
+
+fn main() {
+    let body = r##"
+        {
+            "objects": [
+                "catalog"
+            ]
+        }
+    "##;
+
+    dbg!(&body);
+
+    let access_token = env!("ACCESS_TOKEN");
+    let client_token = env!("CLIENT_TOKEN");
+    let client_secret = env!("CLIENT_SECRET");
+
+    let client = Client::builder().build().unwrap();
+
+    let base = "https://akab-v6etn6gnkjrsrkuu-ivl4pk6pkkr3yfox.purge.akamaiapis.net";
+    let url_string = format!("{}/ccu/v3/invalidate/tag/{}", base, "production");
+    let url = Url::parse(&url_string).unwrap();
+
+    let auth_data =
+        authorization_header(
+            HttpMethod::Post,
+            &url,
+            Some(body.as_bytes()),
+            access_token,
+            client_token,
+            client_secret
+        );
+
+    let req = client
+        .post(url)
+        .header(header::AUTHORIZATION, auth_data)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(body)
+        .build()
+        .unwrap();
+
+    dbg!(&req);
+
+    let mut res = client.execute(req).unwrap();
+
+    println!("{:#?}", res);
+    println!("{}", res.text().unwrap());
+}
